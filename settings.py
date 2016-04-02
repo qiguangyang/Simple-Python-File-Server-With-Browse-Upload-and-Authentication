03cafa9e14b399f8514652d05451b17a5a04ca46
qiguangyang <qiguangyang@users.noreply.github.com>
2016-04-02 23:08:30 +1030

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 # change these values as you see fit
 # after running `sudo ./install`, the server should run at http://host:port/base_url
-host = '127.0.0.1'
-port = 5000
-base_url = 'some_preferred_url/'
+host = '0.0.0.0' # for public request
+port = 8000
+base_url = 'index/'
 username = 'admin'
 password = 'mypassword'
 
-# for example, with above default values, the server should run at 127.0.0.1:5000/some_preferred_url/
+# for example, with above default values, the server should run at 127.0.0.1:5000/index/
